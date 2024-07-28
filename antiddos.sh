c2ba6e57d765b62a398f7c40cba055b8c8195f7a
Reignsia <142000679+Reignsia@users.noreply.github.com>
2024-07-28 18:13:40 +0800

    Update antiddos.sh

--- antiddos.sh
+++ antiddos.sh
@@ -5,10 +5,11 @@ sudo ufw allow proto tcp from any to any
 sudo ufw allow proto udp from any to any
 sudo ufw default allow outgoing
 sudo ufw enable
-sudo systemctl restart fastnetmon
 sudo service fastnetmon start
+sudo systemctl start fastnetmon
 sudo service fastnetmon restart
-echo "Fastnetmon and UFW is Enabled/Restarted"
+sudo systemctl restart fastnetmon
+echo "======| Fastnetmon and UFW is Enabled/Restarted |======"
 sleep 5
 # Configure FastNetMon settings
 sudo fcli set main networks_list 11.22.33.0/22
@@ -16,7 +17,7 @@ sudo fcli set main networks_list 0.0.0.0/1
 sudo fcli set main networks_list 127.0.0.1/1
 sudo fcli set main networks_list beef::1/64
 sudo fcli commit
-echo "Finished network list"
+echo "======| Finished network list |======="
 sleep 5
 
 sudo fcli set main netflow enable
@@ -24,7 +25,9 @@ sudo fcli set main netflow_ports 2055
 sudo fcli set main netflow_host 0.0.0.0
 sudo fcli set main netflow_host ::
 sudo fcli commit
-echo "Finished netflow main set"
+sudo service fastnetmon restart
+sudo systemctl restart fastnetmon
+echo "======| Finished netflow main set |======"
 sleep 5
 
 sudo fcli set main netflow_sampling_ratio 1
@@ -45,7 +48,9 @@ sudo fcli commit
 
 sudo fcli set main netflow_multi_thread_mode random
 sudo fcli commit
-echo "Finished netflow configuration"
+sudo service fastnetmon restart
+sudo systemctl restart fastnetmon
+echo "======| Finished netflow configuration |======"
 sleep 5
 # Update and install Softflowd
 sudo apt update
@@ -60,6 +65,7 @@ echo -e "netflow = on\nnetflow_port = 2055" | sudo tee -a /etc/init/fastnetmon.c
 # Start and restart FastNetMon service
 sudo service fastnetmon start
 sudo service fastnetmon restart
+sudo systemctl restart fastnetmon
 
 # Create Softflowd systemd service file
 sudo bash -c 'cat <<EOF > /etc/systemd/system/softflowd.service
