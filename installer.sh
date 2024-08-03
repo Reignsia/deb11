033f41db7691baad678a12eb5f351c85ff00a0ee
Reignsia <142000679+Reignsia@users.noreply.github.com>
2024-08-03 22:15:31 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,7 @@
+
 #!/bin/bash
 
-echo "[1/5] Installing Dependencies..."
+echo "[1/5] Installing dependencies..."
 sudo apt-get update -qq > /dev/null 2>&1
 sudo apt-get upgrade -y -qq > /dev/null 2>&1
 sudo dpkg --add-architecture i386 > /dev/null 2>&1
@@ -14,3 +15,5 @@ sudo apt-get install -y -qq lib32gcc-s1 > /dev/null 2>&1
 sudo apt-get install -y -qq libhttp-daemon-perl > /dev/null 2>&1
 sudo apt-get install -y -qq apache2 curl subversion php8.1 php8.1-gd php8.1-zip libapache2-mod-php8.1 php8.1-curl php8.1-mysql php8.1-xmlrpc php-pear mariadb-server-10.6 php8.1-mbstring git php-bcmath > /dev/null 2>&1
 
+echo "[2/5] Configuring..."
+sudo sed -i "s/^bind-address.*/bind-address=0.0.0.0/g" "/etc/mysql/mariadb.conf.d/50-server.cnf"
