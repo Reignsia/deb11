8f9ada13d67d1c8bdc9f2b0acc831748dea50992
Reignsia <142000679+Reignsia@users.noreply.github.com>
2024-08-11 14:57:09 +0800

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -13,3 +13,12 @@ sudo wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/
 sudo dpkg -i "ogp-agent-latest.deb"
 
 (crontab -l 2>/dev/null; echo "0 * * * * echo 3 > /proc/sys/vm/drop_caches") | crontab -
+
+iptables -A INPUT -p tcp --dport 1:65535 -j ACCEPT
+iptables -A INPUT -p udp --dport 1:65535 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 1:65535 -j ACCEPT
+iptables -A OUTPUT -p udp --dport 1:65535 -j ACCEPT
+
+iptables-save > /etc/iptables/rules.v4
+
+sudo cat /root/ogp_user_password
